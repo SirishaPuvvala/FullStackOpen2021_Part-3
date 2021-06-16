9e17846b61421a0b60dd5abe9651a150c9d1525f
Sirisha <55184027+sirisha1610@users.noreply.github.com>
2021-06-16 08:57:38 +0530

    Update note.js

--- phonebook/models/note.js
+++ phonebook/models/note.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
-
 console.log('connecting to', url)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => {
@@ -10,13 +9,11 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
-
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
 })
-
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -24,5 +21,4 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
 module.exports = mongoose.model('Note', noteSchema)
